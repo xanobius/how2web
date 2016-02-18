e1063d0b15bb3138b8330f0fda8c3c8536c1703d
Philipp Lauwiner <philipp@lauwiner.name>
2016-02-18 12:01:16 +0100

    added proper header and footer to lesson 01

--- lesson_01.php
+++ lesson_01.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="de">
-<head>
-
-<meta charset="utf-8">
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
-
-
-</head>
-<body>
+<?php
+    // Include the Header for correct HTML Output
+$title = 'Lesson 01';
+include('html_header.php');
+?>
 
     <form action="" method="post">
         <select name="type">
@@ -20,7 +15,7 @@
         <input type="submit" value="speichern">
     </form>
 
-<div class="container">
+<div class="container newclass">
     <div class="row">
         <div class="col-lg-4">
             <h3>
@@ -55,10 +50,9 @@
                 </ul>
         </div>
     </div>
-
 </div>
 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
-
-</body>
-</html>
+<?php
+    // Include the Footer, incl. bootstrap javascript
+include('html_footer.php');
+?>
